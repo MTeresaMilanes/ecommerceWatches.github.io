da4dcd100d7bc35a32c98402fc5f520bf25b114a
MTeresaMilanes <m.milanes.barroso@accenture.com>
2022-05-05 13:52:46 +0200

    udpate

--- js/scriptGA.js
+++ js/scriptGA.js
@@ -1,5 +1,5 @@
 function myFunction(){
-  document.getElementById("logoIcon").style.color = 'red';
+  document.getElementById("logoIcon").innerHTML = 'Icon Changed' + new Date().toTimeString();
   if(window.dataLayer){
     dataLayer.push({
       'color': 'red',
