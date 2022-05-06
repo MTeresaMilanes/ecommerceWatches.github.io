0341f232e6c7f4bb87ac388be546ec94aaedfeb5
MTeresaMilanes <m.milanes.barroso@accenture.com>
2022-05-06 14:49:19 +0200

    update

--- js/scriptGA.js
+++ js/scriptGA.js
@@ -1,10 +1,3 @@
-function myFunction(){
-  document.getElementById("logoIcon").innerHTML = 'Icon Changed' + new Date().toTimeString();
-  if(window.dataLayer){
-    dataLayer.push({
-      'color': 'red',
-      'conversionValue': 50,
-      'event': 'customize'
-    });
-  }
-}
+
+
+
