f38c21a5665160d0f328d2a3987f7da7fdf3001d
MTeresaMilanes <m.milanes.barroso@accenture.com>
2022-05-09 15:35:29 +0200

    update

--- js/scriptGA.js
+++ js/scriptGA.js
@@ -1,3 +1,40 @@
+var player;
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player('player', {
+      videoId: 'I8AyFs2uESg',
+      events: {
+          // 'onReady': onPlayerReady,
+          'onStateChange': onPlayerStateChange
+      }
+  });
+}
+
+function onPlayerStateChange(event) {
+    if (event.data == YT.PlayerState.PLAYING) {
+      window.dataLayer = window.dataLayer || [];
+      dataLayer.push({
+        'event': 'video_play',
+        'pagePath': 'https://mteresamilanes.github.io/ecommerceWatches.github.io/',
+        'pageTitle': 'ECommerce',
+        'visitorType': 'user',
+      })
+      console.log("event play detected");
+    }
+    if (event.data == YT.PlayerState.PAUSED) {
+      window.dataLayer = window.dataLayer || [];
+      dataLayer.push({
+        'event': 'video_pause',
+        'pagePath': 'https://mteresamilanes.github.io/ecommerceWatches.github.io/',
+        'pageTitle': 'ECommerce',
+        'visitorType': 'user',
+      })
+        console.log("event paused detected");
+    }
+}
+
+// function onPlayerReady(event) { 
+//     event.target.playVideo(); 
+// }
 
 
 
